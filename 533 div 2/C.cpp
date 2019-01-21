e4dfcd59a94be53e5901f9e4e76f750041683edb
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-01-21 18:55:32 +0530

    C solved

--- 533 div 2/C.cpp
+++ 533 div 2/C.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 typedef long long ll;
 
-ll n,l,r;
+int n,l,r;
 
 int mod = 1000000007;
 
 int dp[3][200001];
 
-ll occr[3];
+int occr[3];
 
 ll calc(int sum,int counter) {
     if(dp[sum][counter]!=-1)
@@ -28,14 +28,28 @@ ll calc(int sum,int counter) {
     return dp[sum][counter]=res;
 }
 
+ll kCalc(ll r,ll l) {
+    return r-l+1;
+}
+
 int main() {
     cin>>n>>l>>r;
 
     memset(occr,0,sizeof(occr));
     memset(dp,-1,sizeof(dp));
 
-    for(ll i=l;i<=r;i++)
-      occr[i%3]++;
+    // for(ll i=l;i<=r;i++)
+    //   occr[i%3]++;
+
+    // for(int i=0;i<3;i++)
+    //     cout<<occr[i]<<" ";
+    // cout<<endl;
+
+    for (int i=0;i<3;++i) occr[i]=(r-i+3)/3-(l-i+2)/3;
+
+    // for(int i=0;i<3;i++)
+    //     cout<<occr[i]<<" ";
+    // cout<<endl;
 
     ll res=0;
     for(int i=0;i<3;i++)
