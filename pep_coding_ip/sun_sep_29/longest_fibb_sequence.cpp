4f59dc1cc0a85d50cabeb28fbb2a32a9df6d82b0
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-10-20 17:18:17 +0530

    solved: longest fibonnaci sequence

---
+++ pep_coding_ip/sun_sep_29/longest_fibb_sequence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        int n=A.size();
+        
+        if(!n) {
+            return 0;
+        }
+        
+        if(n==1) {
+            return 0;
+        }
+        
+        if(n==2) {
+            return 0;
+        }
+        
+        unordered_map<int,int> valuePos;
+        for(int i=0;i<n;i++) {
+            valuePos.insert(make_pair(A[i],i));
+        }
+        
+        int dp[n][n]={0};
+        
+        dp[0][0]=0;
+        
+        dp[1][0]=2;
+        dp[1][1]=0;
+        
+        
+        int max_length=INT_MIN;
+        for(int i=2;i<n;i++) {
+            for(int j=i;j>=0;j--) {
+                if(i==j) {
+                    dp[i][j]=0;
+                } else {
+                    if(A[i]-A[j]<A[j] && valuePos.find(A[i]-A[j])!=valuePos.end()) {
+                        dp[i][j]=dp[j][valuePos[A[i]-A[j]]]+1;
+                    } else {
+                        dp[i][j]=2;
+                    }
+                }
+                max_length=max(max_length,dp[i][j]);
+            }
+        }
+        
+        if(max_length<3) {
+            max_length=0;
+        }
+        
+        return max_length;
+    }
+};
