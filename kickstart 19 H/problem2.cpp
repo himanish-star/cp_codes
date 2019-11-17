4a67d804c8826bebf93c84c0f84cc48c1cc803a5
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2019-11-17 12:22:33 +0530

    draft working, needs more optimization

---
+++ kickstart 19 H/problem2.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool possible;
+
+void dfs(int arr[],bool add,int sum,int used,int t) {
+    int noMoves=0;
+    for(int i=0;i<9;i++) {
+        if(arr[i]>0) {
+            arr[i]--;
+            
+            int vadd;
+            
+            if(add)
+                vadd=i+1;
+            else
+                vadd=-(i+1);
+
+            dfs(arr,!add,sum+vadd,used+1,t);
+            
+            arr[i]++;
+        } {
+            noMoves++;
+        }
+    }
+    if(noMoves==9 && used==t) {
+        if(sum%11==0)
+            possible=true;
+    }
+}
+
+int main() {
+    int t;
+    cin>>t;
+
+    int count=1;
+    while(t--) {
+        int arr[9];
+        int total_digits=0;
+        for(int i=0;i<9;i++) {
+            cin>>arr[i];
+            total_digits+=arr[i];    
+        }
+        
+        possible=false;
+        dfs(arr,true,0,0,total_digits);
+
+        cout<<"Case #"<<count++<<": ";
+        if(possible)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
