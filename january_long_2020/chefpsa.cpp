bda9f6a3dee16cbef32d58195918943f93fc9ef8
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-01-05 14:24:40 +0530

    partially solved: TLE = 4 CHEFPSA.cpp

--- january_long_2020/chefpsa.cpp
+++ january_long_2020/chefpsa.cpp
@@ -6,6 +6,44 @@ typedef long long ll;
 
 #define mod 1000000007
 
+ll gcdExtended(ll a, ll b, ll *x, ll *y) 
+{ 
+    if (a == 0) 
+    { 
+        *x = 0, *y = 1; 
+        return b; 
+    } 
+  
+    ll x1, y1;
+    ll gcd = gcdExtended(b%a, a, &x1, &y1); 
+  
+    *x = y1 - (b/a) * x1; 
+    *y = x1; 
+  
+    return gcd; 
+}
+
+ll modInverse(ll b, ll m) 
+{ 
+    ll x, y; 
+    ll g = gcdExtended(b, m, &x, &y); 
+  
+    if (g != 1) 
+        return -1; 
+  
+    return (x%m + m) % m; 
+} 
+  
+ll modDivide(ll a, ll b, ll m) 
+{ 
+    a = a % m; 
+    ll inv = modInverse(b, m); 
+    if (inv == -1) 
+       return 0; 
+    else
+       return (inv * a) % m; 
+} 
+
 int main() {
     int t;
     cin>>t;
@@ -15,7 +53,7 @@ int main() {
         cin>>n;
 
         vector<ll> x(2*n);
-        map<ll,ll> occr;
+        unordered_map<ll,ll> occr;
         ll gsum=0;
         ll allZero=0;
         for(ll i=0;i<2*n;i++) {
@@ -34,14 +72,23 @@ int main() {
         }
 
         map<pair<ll,ll>,ll> cnts;
-        set<pair<ll,ll>> unq;
+        bool check=true;
+        // set<pair<ll,ll>> unq;
         for(ll i=0;i<x.size();i++) {
             if(occr[x[i]]<=0)
                 continue;
 
+            if(tsum==x[i] && check && occr[x[i]]>=2) {
+                nop+=2;
+                occr[x[i]]-=2;
+                // occr[0]-=2;
+                check=false;
+                continue;
+            }
+
             if(x[i]==tsum-x[i] && occr[x[i]]>=2) {
                 occr[x[i]]-=2;
-                unq.insert(make_pair(x[i],x[i]));
+                // unq.insert(make_pair(x[i],x[i]));
                 cnts[make_pair(x[i],x[i])]++;
                 nop++;
                 continue;
@@ -55,17 +102,11 @@ int main() {
                 
                 ll v1=min(x[i],tsum-x[i]);
                 ll v2=max(x[i],tsum-x[i]);
-                unq.insert(make_pair(v1,v2));
+                // unq.insert(make_pair(v1,v2));
                 cnts[make_pair(v1,v2)]++;
                 continue;
             }
             
-            if(tsum==x[i] && occr[x[i]]>=1) {
-                nop++;
-                occr[x[i]]--;
-                occr[0]--;
-                continue;
-            }
         }
 
         // printf("nop -> %lld\n",nop);
@@ -76,7 +117,7 @@ int main() {
 
         //factorial
         ll fact=1;
-        map<ll,ll> mp;
+        unordered_map<ll,ll> mp;
         for(ll i=1;i<=nop-2;i++) {
             fact=(fact*i)%mod;
             mp[i]=fact;
@@ -84,11 +125,14 @@ int main() {
         
         ans=(ans*fact)%mod;
 
-        for(set<pair<ll,ll>>::iterator it=unq.begin();it!=unq.end();it++) {
-            ll v1=it->first;
+        for(map<pair<ll,ll>,ll>::iterator it=cnts.begin();it!=cnts.end();it++) {
+            // ll v1=it->first;
             ll v2=it->second;
             // cout<<"mp"<<mp[cnts[make_pair(v1,v2)]]<<endl;
-            ans/=mp[cnts[make_pair(v1,v2)]];
+            ans=modDivide(ans,mp[v2],mod);
+            
+            if(ans<0)
+                ans+=mod;
         }
 
         cout<<ans<<endl;
