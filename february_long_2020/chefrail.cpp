770975087dae98e2b4c772b9cdcf775b86d1a13f
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-02-15 23:21:20 +0530

    some more

--- february_long_2020/chefrail.cpp
+++ february_long_2020/chefrail.cpp
@@ -4,10 +4,45 @@ using namespace std;
 
 typedef long long ll;
 
+#define MAXN   100001 
+
+int spf[MAXN]; 
+void sieve() 
+{ 
+    spf[1] = 1; 
+    for (int i=2; i<MAXN; i++) 
+        spf[i] = i; 
+
+    for (int i=4; i<MAXN; i+=2) 
+        spf[i] = 2; 
+  
+    for (int i=3; i*i<MAXN; i++) 
+    { 
+        if (spf[i] == i) 
+        { 
+            for (int j=i*i; j<MAXN; j+=i) 
+                if (spf[j]==j) 
+                    spf[j] = i; 
+        } 
+    } 
+} 
+  
+vector<int> getFactorization(int x) 
+{ 
+    vector<int> ret; 
+    while (x != 1) 
+    { 
+        ret.push_back(spf[x]); 
+        x = x / spf[x]; 
+    } 
+    return ret; 
+}
+
 int main() {
     ll t;
     cin>>t;
 
+    sieve();
     while(t--) {
         ll n,m;
         cin>>n>>m;
@@ -85,7 +120,7 @@ int main() {
             ans+=(n-nxz)*(m-nyz);
         }
         
-        cout<<ans<<endl;
+        printf("%lld\n",ans);
     }
     return 0;
 }
