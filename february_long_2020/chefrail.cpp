eafc956b25ada644ab9c2d9b2dfbc407b9686a10
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-03-07 10:07:30 +0530

    changes which need to be monitored later

--- february_long_2020/chefrail.cpp
+++ february_long_2020/chefrail.cpp
@@ -4,45 +4,25 @@ using namespace std;
 
 typedef long long ll;
 
-#define MAXN   100001 
-
-int spf[MAXN]; 
-void sieve() 
-{ 
-    spf[1] = 1; 
-    for (int i=2; i<MAXN; i++) 
-        spf[i] = i; 
-
-    for (int i=4; i<MAXN; i+=2) 
-        spf[i] = 2; 
-  
-    for (int i=3; i*i<MAXN; i++) 
-    { 
-        if (spf[i] == i) 
-        { 
-            for (int j=i*i; j<MAXN; j+=i) 
-                if (spf[j]==j) 
-                    spf[j] = i; 
-        } 
-    } 
-} 
-  
-vector<int> getFactorization(int x) 
-{ 
-    vector<int> ret; 
-    while (x != 1) 
-    { 
-        ret.push_back(spf[x]); 
-        x = x / spf[x]; 
-    } 
-    return ret; 
-}
-
 int main() {
     ll t;
     cin>>t;
 
-    sieve();
+    vector<ll> allFactors[100001];
+    for(int i=1;i<=100000;i++) {
+        ll num=i*i;
+        for(int j=1;j*j<=i;j++) {
+            if(num%j==0) {
+                if(num/j==j) {
+                    allFactors[i].push_back(j); 
+                } else {
+                    allFactors[i].push_back(j);
+                    allFactors[i].push_back(num/j);
+                }
+            }
+        }
+    }
+
     while(t--) {
         ll n,m;
         cin>>n>>m;
@@ -60,55 +40,107 @@ int main() {
         for(ll i=0;i<n;i++) {
             if(arr1[i]==0)
                 nxz++;
-            // sqr1[i]=arr1[i]*arr1[i];
             um1[arr1[i]]++;
-            // prllf("(%d,sqr: %d,cnt: %d)",arr1[i],sqr1[i],um1[sqr1[i]]);
         }
-        // cout<<endl;
         
         for(ll i=0;i<m;i++) {
             if(arr2[i]==0)
                 nyz++;
-            // sqr2[i]=arr2[i]*arr2[i];
             um2[arr2[i]]++;
-            // prllf("(%d,sqr: %d,cnt: %d)",arr2[i],sqr2[i],um2[sqr2[i]]);
         }
-        // cout<<endl;
 
         ll ans=0;
         for(ll i=0;i<n;i++) {
             ll num=arr1[i]*arr1[i];
-            for(ll j=1;j<=sqrt(num);j++) {
+            for(ll j=1;j*j<=abs(arr1[i]);j++) {
                 if(num%j==0) {
                     if (num/j == j) {
                         if(um2[j] && um2[-j]) {
+                            um2[j]--;
+                            um2[-j]--;
+                            ans++;
+                        }
+                    } else if(abs(arr1[i])/j==j) {
+                        if(um2[j] && um2[-num/j]) {
+                            um2[j]--;
+                            um2[-num/j]--;
+                            ans++;
+                        } 
+                        if(um2[-j] && um2[num/j]) {
+                            um2[-j]--;
+                            um2[num/j]--;
                             ans++;
                         }
                     } else {
                         if(um2[j] && um2[-num/j]) {
+                            um2[j]--;
+                            um2[-num/j]--;
                             ans++;
                         } 
                         if(um2[-j] && um2[num/j]) {
+                            um2[-j]--;
+                            um2[num/j]--;
                             ans++;
                         }
-                    } 
+
+                        ll ld=abs(arr1[i])/j;
+                        if(um2[ld] && um2[-num/ld]) {
+                            um2[ld]--;
+                            um2[-num/ld]--;
+                            ans++;
+                        } 
+                        if(um2[-ld] && um2[num/ld]) {
+                            um2[-ld]--;
+                            um2[num/ld]--;
+                            ans++;
+                        }
+                    }
                 }
             }
         }
         
         for(ll i=0;i<m;i++) {
             ll num=arr2[i]*arr2[i];
-            for(ll j=1;j<=sqrt(num);j++) {
+            for(ll j=1;j*j<=abs(arr2[i]);j++) {
                 if(num%j==0) {
                     if (num/j == j) {
                         if(um1[j] && um1[-j]) {
+                            um1[j]--;
+                            um1[-j]--;
+                            ans++;
+                        }
+                    } else if(abs(arr2[i])/j==j) {
+                        if(um1[j] && um1[-num/j]) {
+                            um1[j]--;
+                            um1[-num/j]--;
+                            ans++;
+                        } 
+                        if(um1[-j] && um1[num/j]) {
+                            um1[-j]--;
+                            um1[num/j]--;
                             ans++;
                         }
                     } else {
                         if(um1[j] && um1[-num/j]) {
+                            um1[j]--;
+                            um1[-num/j]--;
                             ans++;
                         } 
                         if(um1[-j] && um1[num/j]) {
+                            um1[-j]--;
+                            um1[num/j]--;
+                            ans++;
+                        }
+
+                        ll ld=abs(arr2[i])/j;
+                        if(um1[ld] && um1[-num/ld]) {
+                            um1[ld]--;
+                            um1[-num/ld]--;
+                            ans++;
+                        } 
+                        if(um1[-ld] && um1[num/ld]) {
+                            um1[-ld]--;
+                            um1[num/ld]--;
                             ans++;
                         }
                     } 
--- pep_coding_ip/README.md
+++ pep_coding_ip/README.md
@@ -259,8 +259,8 @@
 - [ ] 2. `Adapters`
 - [x] 3. `Remove K digits From number` https://leetcode.com/problems/remove-k-digits/
 - [x] 4. `Car fleet` https://leetcode.com/problems/car-fleet/
-- [ ] 5. `First negative Integer in k sized window`
-- [ ] 6. `Addition BINADD`
+- [ ] 5. `First negative Integer in k sized window` https://www.geeksforgeeks.org/first-negative-integer-every-window-size-k/
+- [ ] 6. `Addition BINADD` https://www.codechef.com/DEC19A/problems/BINADD
 
 #### Sat, Dec 28/2019
 - [x] 1. `Gas Station` https://leetcode.com/problems/gas-station/
