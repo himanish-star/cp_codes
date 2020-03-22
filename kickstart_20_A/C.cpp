61e5eeb1c86e46aa544b1a964fb5386141a5060d
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2020-03-22 17:50:46 +0530

    some new codes

---
+++ kickstart_20_A/C.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll t;
+    cin>>t;
+
+    ll id=1;
+    while(t--) {
+        ll n,k;
+        cin>>n>>k;
+
+        vector<ll> times(n);
+        for(ll i=0;i<n;i++)
+            cin>>times[i];
+
+        priority_queue<pair<ll,pair<ll,ll>>> pq;
+        for(ll i=0;i<n-1;i++) {
+            pq.push({times[i+1]-times[i],{times[i],times[i+1]}});
+        }
+        
+        for(ll i=0;i<k;i++) {
+            pair<ll,pair<ll,ll>> t=pq.top();
+            if(t.first==1)
+                break;
+            pq.pop();
+            ll mid=(t.second.first+t.second.second)/2;
+            pq.push({mid-t.second.first,{t.second.first,mid}});
+            pq.push({t.second.second-mid,{mid,t.second.second}});
+        }
+
+        printf("Case #%lld: %lld\n",id,pq.top().first);
+        // vector<ll> res;
+        // while(pq.size()) {
+        //     pair<ll,pair<ll,ll>> t=pq.top();
+        //     pq.pop();
+        //     res.push_back(t.second.first);
+        // }
+
+        // sort(res.begin(),res.end());
+        // for(int i=0;i<res.size();i++) {
+        //     cout<<res[i]<<" ";
+        // }
+        // cout<<endl;
+        id++;
+    }
+}
